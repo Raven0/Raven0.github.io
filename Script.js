a3d80267fff3623de55e659a0fac688d029b1cc7
Muhammad Rayandika <mrayandika15@gmail.com>
2021-05-06 14:25:05 +0700

    Second
    Task :
    1.Layouting/Reponsive Size
    3.Social Media In bottom

---
+++ Script.js
@@ -0,0 +1,151 @@
+
+// WALLET CODE
+    const popup1 = document.getElementById("popup-1");
+    const popup2 = document.getElementById("popup-2");
+    const popup3 = document.getElementById("popup-3");
+
+    function togglePopup1(){
+        popup1.classList.toggle("active");
+    };
+
+    function togglePopup2(){
+        popup2.classList.toggle("active");
+    };
+
+    function togglePopup3(){
+        popup3.classList.toggle("active");
+    };
+
+// SMART RENJA
+    const popup4 = document.getElementById("popup-4");
+    const popup5 = document.getElementById("popup-5");
+    const popup6 = document.getElementById("popup-6");
+
+    function togglePopup4(){
+        popup4.classList.toggle("active");
+    };
+    function togglePopup5(){
+        popup5.classList.toggle("active");
+    };
+    function togglePopup6(){
+        popup6.classList.toggle("active");
+    };
+
+// SIBU PERSINDO
+const popup7 = document.getElementById("popup-7");
+const popup8 = document.getElementById("popup-8");
+const popup9 = document.getElementById("popup-9");
+
+    function togglePopup7(){
+        popup7.classList.toggle("active");
+    };
+    function togglePopup8(){
+        popup8.classList.toggle("active");
+    };
+    function togglePopup9(){
+        popup9.classList.toggle("active");
+    };
+
+// SEROJA
+
+const popup10 = document.getElementById("popup-10");
+const popup11 = document.getElementById("popup-11");
+const popup12 = document.getElementById("popup-12");
+
+    function togglePopup10(){
+        popup10.classList.toggle("active");
+    };
+    function togglePopup11(){
+        popup11.classList.toggle("active");
+    };
+    function togglePopup12(){
+        popup12.classList.toggle("active");
+    };
+
+// ANGGOTA PERSINJO
+const popup13 = document.getElementById("popup-13");
+const popup14 = document.getElementById("popup-14");
+
+
+    function togglePopup13(){
+        popup13.classList.toggle("active");
+    };
+    function togglePopup14(){
+        popup14.classList.toggle("active");
+    };
+
+// YELLOW TRUCK
+const popup15 = document.getElementById("popup-15");
+const popup16 = document.getElementById("popup-16");
+const popup17 = document.getElementById("popup-17");
+
+    function togglePopup15(){
+        popup15.classList.toggle("active");
+    };
+    function togglePopup16(){
+        popup16.classList.toggle("active");
+    };
+    function togglePopup17(){
+        popup17.classList.toggle("active");
+    };
+
+// AIWA APPS
+
+const popup18 = document.getElementById("popup-18");
+const popup19 = document.getElementById("popup-19");
+const popup20 = document.getElementById("popup-20");
+
+    function togglePopup18(){
+        popup18.classList.toggle("active");
+    };
+    function togglePopup19(){
+        popup19.classList.toggle("active");
+    };
+    function togglePopup20(){
+        popup20.classList.toggle("active");
+    };
+
+// PKB
+
+const popup21 = document.getElementById("popup-21");
+const popup22 = document.getElementById("popup-22");
+
+
+    function togglePopup21(){
+        popup21.classList.toggle("active");
+    };
+    function togglePopup22(){
+        popup22.classList.toggle("active");
+    };
+
+// FINANCE SYSTEM
+
+const popup23 = document.getElementById("popup-23");
+const popup24 = document.getElementById("popup-24");
+
+
+    function togglePopup23(){
+        popup23.classList.toggle("active");
+    };
+    function togglePopup24(){
+        popup24.classList.toggle("active");
+    };
+
+// STUDENT AFFAIR
+
+const popup25 = document.getElementById("popup-25");
+const popup26 = document.getElementById("popup-26");
+const popup27 = document.getElementById("popup-27");
+
+
+    function togglePopup25(){
+        popup25.classList.toggle("active");
+    };
+    function togglePopup26(){
+        popup26.classList.toggle("active");
+    };
+    function togglePopup27(){
+        popup27.classList.toggle("active");
+    };
+
+
